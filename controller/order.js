c0bf3223dfdc341869f346a827a65f81f34b4fb0
yashisHimsara <139766123+yashisHimsara@users.noreply.github.com>
2024-06-11 23:14:11 +0530

    Update order.js

--- controller/order.js
+++ controller/order.js
@@ -63,12 +63,12 @@ loadAllCustomerId();
 generateCurrentDate();
 
 $('#orderFormCstId').on('change', function(){
-    selectedCustomerId = $('#cusIdOption option:selected').text();
+    selectedCustomerId = $('#orderFormCstId option:selected').text();
     for (let customerArElement of customer) {
         if (customerArElement.id==selectedCustomerId){
-            $('#orderFormCustomerName').val(customerArElement.name);
-            $('#orderFormCustomerSalary').val(customerArElement.salary);
-            $('#orderFormCustomerAddress').val(customerArElement.address);
+            $('#orderFormCusName').val(customerArElement.name);
+            $('#orderFormCusSalary').val(customerArElement.salary);
+            $('#orderFormCusAddress').val(customerArElement.address);
         }
     }
 });
@@ -83,7 +83,7 @@ $('#orderFormItemId').on('change', function(){
 
             $('#orderFormItemName').val(itemName);
             $('#orderFormPrice').val(itemPrice);
-            $('#orderFormQty').val(itemQty);
+            $('#orderFormQtyOnHand').val(itemQty);
         }
     }
 });
@@ -121,47 +121,47 @@ $("#btnAddItem").on('click', () => {
 
 });
 
-function updateQty(){
-    var orderFormQtyOnHand=$('#orderFormQtyOnHand').val();
-    var updateQty=orderFormQtyOnHand-orderQty;
-
-    let selectedItemIndex = items.findIndex(item => item.id === selectedItemId);
-    if (selectedItemIndex !== -1) {
-        items[selectedItemIndex].qty = updateQty;
-        $('#orderFormQtyOnHand').val(updateQty);
-    }
-}
-
-function calTotalAllItem(){
-    var totalAllItems = 0;
-    order.forEach(item => {
-        totalAllItems += item.total;
-    });
-
-    $('#total').val(totalAllItems);
-    $('#subTotal').val(totalAllItems);
-}
-
-$("#orderQty").on('keyup', () => {
-    var orderFormQtyOnHand=parseInt($('#orderFormQtyOnHand').val());
-    var orderQty =parseInt($('#orderQty').val());
-    var itemQtyPattern = /^\d+$/;
-    var errorMessageQty = $('.errorOrderQty');
-    var errorQty = $('.errorQty');
-
-
-    if (!itemQtyPattern.test(orderQty)) {
-        errorQty.show();
-        $('#orderQty').css('border', '2px solid red');
-    } else {
-        errorQty.hide();
-        $('#orderQty').css('border', '2px solid green');
-    }
-
-    if (orderQty>orderFormQtyOnHand){
-        $('#orderQtyValue').text(orderFormQtyOnHand);
-        errorMessageQty.show();
-    }else {
-        errorMessageQty.hide();
-    }
-});
+// function updateQty(){
+//     var orderFormQtyOnHand=$('#orderFormQtyOnHand').val();
+//     var updateQty=orderFormQtyOnHand-orderQty;
+//
+//     let selectedItemIndex = items.findIndex(item => item.id === selectedItemId);
+//     if (selectedItemIndex !== -1) {
+//         items[selectedItemIndex].qty = updateQty;
+//         $('#orderFormQtyOnHand').val(updateQty);
+//     }
+// }
+//
+// function calTotalAllItem(){
+//     var totalAllItems = 0;
+//     order.forEach(item => {
+//         totalAllItems += item.total;
+//     });
+//
+//     $('#total').val(totalAllItems);
+//     $('#subTotal').val(totalAllItems);
+// }
+//
+// $("#orderQty").on('keyup', () => {
+//     var orderFormQtyOnHand=parseInt($('#orderFormQtyOnHand').val());
+//     var orderQty =parseInt($('#orderQty').val());
+//     var itemQtyPattern = /^\d+$/;
+//     var errorMessageQty = $('.errorOrderQty');
+//     var errorQty = $('.errorQty');
+//
+//
+//     if (!itemQtyPattern.test(orderQty)) {
+//         errorQty.show();
+//         $('#orderQty').css('border', '2px solid red');
+//     } else {
+//         errorQty.hide();
+//         $('#orderQty').css('border', '2px solid green');
+//     }
+//
+//     if (orderQty>orderFormQtyOnHand){
+//         $('#orderQtyValue').text(orderFormQtyOnHand);
+//         errorMessageQty.show();
+//     }else {
+//         errorMessageQty.hide();
+//     }
+// });
